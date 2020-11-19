707be3107899ea14f7cea2e6b2cbe417ae820672
Rohit <rohit.duttshyam@gmail.com>
2020-11-19 18:31:56 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var engine, world;
 var box1, pig1;
 var backgroundImg;
 
+var platform
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -17,6 +19,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(150, 350, 300, 80)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -46,10 +49,10 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
+    platform.display();
     pig1.display();
     log1.display();
-
-    box3.display();
+    box3.display()
     box4.display();
     pig3.display();
     log3.display();
